a47f180dc18d5b9e436c639f45862d4e0fc1ccfa
kelvinst <kelvin.stinghen@me.com>
2025-11-22 20:11:34 -0300

    test: add coverage for heex function component calls for aliased modules
    This proves that "jump to definition" works for remote heex component calls
    using the `<Module.fun ...>` pattern.

--- apps/expert/test/engine/code_intelligence/definition_test.exs
+++ apps/expert/test/engine/code_intelligence/definition_test.exs
@@ -161,6 +161,29 @@ defmodule Expert.Engine.CodeIntelligence.DefinitionTest do
     end
   end
 
+  describe "definition/2 when making remote heex component call by alias" do
+    setup [:with_referenced_file]
+
+    test "find the definition of a remote heex component call", %{project: project, uri: referenced_uri} do
+      subject_module = ~q[
+        defmodule UsesPhoenixComponentFunction do
+          alias MyDefinition
+
+          def uses_greet() do
+            ~H"""
+            <MyDefinition.gree|t />
+            """
+          end
+        end
+      ]
+
+      assert {:ok, ^referenced_uri, definition_line} =
+               definition(project, subject_module, referenced_uri)
+
+      assert definition_line == ~S[  def «greet»(name) do]
+    end
+  end
+
   describe "definition/2 when making remote call by import" do
     setup [:with_referenced_file]
 
